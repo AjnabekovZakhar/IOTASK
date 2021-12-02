d6298653e47b789b74d90485a70ca816acaf0ec6
Захар Айнабеков <st068587@student.spbu.ru>
2021-12-02 15:01:25 +0300

    test

---
+++ IOTASK/IOTASK.cpp
@@ -0,0 +1,73 @@
+
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<fstream>
+#include<vector>
+
+void split(const std::string& s, char delim, std::vector<std::string>& elems) {
+	std::stringstream ss;
+	ss.str(s);
+	std::string item;
+	while (std::getline(ss, item, delim)) {
+		elems.push_back(item);
+	}
+}
+void split_to_double(const std::string& s, char delim, std::vector<double>& elems) {
+	std::stringstream ss;
+	ss.str(s);
+	std::string item;
+	while (std::getline(ss, item, delim)) {
+		elems.push_back(stod(item));
+	}
+}
+int main()
+{
+	std::vector<std::string> names;
+	std::vector<std::vector<double>> values;
+	std::vector<double> temp_values;
+	std::string input_str,temp_str;
+
+	setlocale(LC_ALL, "Russian");
+	//Забавный факт, если включить строчку выше, то stod начнёт воспринимать ',' как разделитель, а не '.'
+	std::ifstream input_file("test.txt");
+	try {
+		if (!input_file.is_open()) {
+			throw std::runtime_error("Не могу открыть файл");
+		}
+
+		std::getline(input_file, input_str);
+
+		split(input_str, '\t', names);
+
+		while (std::getline(input_file, input_str)) {
+			split_to_double(input_str, '\t',temp_values);
+			if(temp_values.size()!=names.size())
+				throw std::length_error("- строка с неправильным количеством элементов");
+			values.push_back(temp_values);
+			temp_values.clear();
+		}
+
+		for (int i = 0; i < names.size(); ++i)
+			std::cout << names[i] << '\t';
+		std::cout << std::endl;
+
+
+		for (int i = 0; i < values.size(); ++i) {
+			for (int j = 0; j < values[i].size(); ++j)
+				std::cout << values[i][j] << '\t';
+			std::cout << std::endl;
+		}
+	}
+	catch (const std::runtime_error& exept) {
+		std::cout << "Исключение: " << values.size() + 1 << ' ' << exept.what() << std::endl;
+	}
+	catch(const std::invalid_argument& exept){//для stod
+		std::cout << "Исключение: " << values.size() + 1 << ' ' << exept.what() << std::endl;
+	}
+	catch (const std::length_error& exept) {
+		std::cout << "Исключение: " <<values.size()+1 <<' '<< exept.what() << std::endl;
+	}
+	return 0;
+}
+
